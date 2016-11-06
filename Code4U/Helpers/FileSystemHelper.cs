2b90d5869cf69b276fe559d44125cb111780b6fa
Lucian <lucian.mbraz@gmail.com>
2016-11-06 15:44:13 -0200

    Implemented funcionality to create git style conflicts between generated and existing files

--- Code4U/Helpers/FileSystemHelper.cs
+++ Code4U/Helpers/FileSystemHelper.cs
@@ -22,5 +22,74 @@ namespace Code4U.Helpers
 
             return directories.ToArray();
         }
+
+        public static void WriteFile(string filename, string fileContent)
+        {
+            var directory = Path.GetDirectoryName(filename);
+
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+
+            if (File.Exists(filename))
+            {
+                var theirsFileContent = File.ReadAllText(filename);
+
+                if (string.IsNullOrEmpty(theirsFileContent))
+                {
+                    File.WriteAllText(filename, fileContent);
+                }
+                else if (!theirsFileContent.Equals(fileContent))
+                {
+                    WriteConflictFile(filename, theirsFileContent, fileContent);
+                }
+            }
+            else
+            {
+                File.WriteAllText(filename, fileContent);
+            }
+        }
+
+        private static void WriteConflictFile(string filename, string theirsFileContent, string mineFileContent)
+        {
+            var theirsLines = theirsFileContent.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+
+            var mineLines = mineFileContent.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+
+            var fileContent = new List<string>();
+
+            var lines = theirsLines.Length;
+
+            if (mineLines.Length > lines) lines = mineLines.Length;
+
+            var beginMark = "<<<<<<< Theirs";
+            var middleMark = "=======";
+            var endMark = ">>>>>>> Mine";
+
+            for (int line = 0; line < lines; line++)
+            {
+                var mineLine = string.Empty;
+                if (mineLines.Length > line) mineLine = mineLines[line];
+
+                var theirsLine = string.Empty;
+                if (theirsLines.Length > line) theirsLine = theirsLines[line];
+
+                if (!theirsLine.Equals(mineLine))
+                {
+                    fileContent.Add(beginMark);
+                    fileContent.Add(theirsLine);
+                    fileContent.Add(middleMark);
+                    fileContent.Add(mineLine);
+                    fileContent.Add(endMark);
+                }
+                else
+                {
+                    fileContent.Add(mineLine);
+                }
+            }
+
+            File.WriteAllLines(filename, fileContent.ToArray());
+        }
     }
 }
